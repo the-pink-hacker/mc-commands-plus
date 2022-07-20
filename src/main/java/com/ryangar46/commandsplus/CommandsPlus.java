c0eec5698a721007283a1ab69b683b9698eb5800
Ryan Garrett <ryangar46@gmail.com>
2022-07-19 20:45:51 -0400

    Add gm alias for gamemode command

--- src/main/java/com/ryangar46/commandsplus/CommandsPlus.java
+++ src/main/java/com/ryangar46/commandsplus/CommandsPlus.java
@@ -1,10 +1,13 @@
 package com.ryangar46.commandsplus;
 
+import com.mojang.brigadier.CommandDispatcher;
 import com.ryangar46.commandsplus.command.argument.ArgumentTypeManager;
 import com.ryangar46.commandsplus.server.command.*;
 import com.ryangar46.commandsplus.server.dedicated.command.CPStopCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -31,7 +34,15 @@ public class CommandsPlus implements ModInitializer {
                 CPStopCommand.register(dispatcher);
             }
 
+            // Aliases
+            createAlias(dispatcher, "gamemode", "gm");
+            createAlias(dispatcher, "help", "?");
+
             LOGGER.info("Registered commands");
         });
+    }
+
+    private static void createAlias(CommandDispatcher<ServerCommandSource> dispatcher, String original, String alias) {
+        dispatcher.register(CommandManager.literal(alias).redirect(dispatcher.getRoot().getChild(original)));
     }
 }
