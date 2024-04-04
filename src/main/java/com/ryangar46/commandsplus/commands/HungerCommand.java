ab128a6262d31b744e94f7e3feea86f39ac47be8
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:12:58 -0400

    Fix hunger command error

--- src/main/java/com/ryangar46/commandsplus/commands/HungerCommand.java
+++ src/main/java/com/ryangar46/commandsplus/commands/HungerCommand.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TranslatableText;
 
 import java.util.Collection;
@@ -17,30 +18,26 @@ public class HungerCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("hunger")
                 .requires((commandSource) -> commandSource.hasPermissionLevel(2))
-                .then(CommandManager.argument("target", EntityArgumentType.entities())
+                .then(CommandManager.argument("target", EntityArgumentType.players())
                         .then(CommandManager.literal("get")
                                 .executes((context) -> {
-                                    return getHunger(context.getSource(), EntityArgumentType.getEntity(context, "target")); }))
+                                    return getHunger(context.getSource(), EntityArgumentType.getPlayer(context, "target")); }))
                         .then(CommandManager.literal("set")
                                 .then(CommandManager.argument("hunger", IntegerArgumentType.integer(0, Integer.MAX_VALUE))
                                         .executes((context) -> {
-                                            return setHunger(context.getSource(), EntityArgumentType.getEntities(context, "target"), IntegerArgumentType.getInteger(context, "health")); })))
+                                            return setHunger(context.getSource(), EntityArgumentType.getPlayers(context, "target"), IntegerArgumentType.getInteger(context, "hunger")); })))
                         .then(CommandManager.literal("add")
                                 .then(CommandManager.argument("hunger", IntegerArgumentType.integer(-Integer.MAX_VALUE, Integer.MAX_VALUE))
                                         .executes((context) -> {
-                                            return addHunger(context.getSource(), EntityArgumentType.getEntities(context, "target"), IntegerArgumentType.getInteger(context, "health")); })))));
+                                            return addHunger(context.getSource(), EntityArgumentType.getPlayers(context, "target"), IntegerArgumentType.getInteger(context, "hunger")); })))));
     }
 
-    private static int setHunger(ServerCommandSource source, Collection<? extends Entity> target, int hunger) throws CommandSyntaxException {
+    private static int setHunger(ServerCommandSource source, Collection<? extends ServerPlayerEntity> target, int hunger) throws CommandSyntaxException {
         int i = 0;
 
-        for (Entity entity : target) {
-            // Checks if they are a Living Entity
-            if (entity instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity)entity;
-                player.getHungerManager().setFoodLevel(hunger);
-                i++;
-            }
+        for (ServerPlayerEntity player : target) {
+            player.getHungerManager().setFoodLevel(hunger);
+            i++;
         }
 
         if (i > 0) {
@@ -53,35 +50,27 @@ public class HungerCommand {
         return i;
     }
 
-    private static int getHunger(ServerCommandSource source, Entity target) throws CommandSyntaxException {
+    private static int getHunger(ServerCommandSource source, ServerPlayerEntity target) throws CommandSyntaxException {
         int hunger = -1;
 
-        // Checks if they are a Living Entity
-        if (target instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity)target;
-            hunger = player.getHungerManager().getFoodLevel();
-        }
+        hunger = target.getHungerManager().getFoodLevel();
 
         if (hunger >= 0) {
-            source.sendFeedback(new TranslatableText("command.hunger.get.success", hunger), true);
+            source.sendFeedback(new TranslatableText("command.hunger.get.success", hunger), false);
         }
         else {
             throw new SimpleCommandExceptionType(new TranslatableText("command.hunger.get.fail")).create();
         }
 
-        return (int)hunger;
+        return hunger;
     }
 
-    private static int addHunger(ServerCommandSource source, Collection<? extends Entity> target, int hunger) throws CommandSyntaxException {
+    private static int addHunger(ServerCommandSource source, Collection<? extends ServerPlayerEntity> target, int hunger) throws CommandSyntaxException {
         int i = 0;
 
-        for (Entity entity : target) {
-            // Checks if they are a Living Entity
-            if (entity instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) entity;
-                player.getHungerManager().setFoodLevel(player.getHungerManager().getFoodLevel() + hunger);
-                i++;
-            }
+        for (ServerPlayerEntity player : target) {
+            player.getHungerManager().setFoodLevel(player.getHungerManager().getFoodLevel() + hunger);
+            i++;
         }
 
         if (i > 0) {
