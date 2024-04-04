08d7c9e97ddd9c270af9991b5927de0ae273b284
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:12:58 -0400

    Add itemDespawnAge gamerule

---
+++ src/main/java/com/ryangar46/commandsplus/mixin/entity/ItemEntityMixin.java
@@ -0,0 +1,46 @@
+package com.ryangar46.commandsplus.mixin.entity;
+
+import com.ryangar46.commandsplus.world.GameRuleManager;
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+    @ModifyConstant(
+            method = "tick()V",
+            constant = @Constant(intValue = 6_000)
+    )
+    private int despawnAgeTick(int age) {
+        return getDespawnAge();
+    }
+
+    @ModifyConstant(
+            method = "canMerge()Z",
+            constant = @Constant(intValue = 6_000)
+    )
+    private int despawnAgeCanMerge(int age) {
+        return getDespawnAge();
+    }
+
+    @ModifyConstant(
+            method = "setCovetedItem()V",
+            constant = @Constant(intValue = -6_000)
+    )
+    private int despawnAgeConvertedItem(int age) {
+        return -getDespawnAge();
+    }
+
+    @ModifyConstant(
+            method = "setDespawnImmediately()V",
+            constant = @Constant(intValue = 5_999)
+    )
+    private int despawnAgeDespawnImmediately(int age) {
+        return getDespawnAge() - 1;
+    }
+
+    private int getDespawnAge() {
+        return ((ItemEntity)(Object)this).world.getGameRules().getInt(GameRuleManager.ITEM_DESPAWN_AGE);
+    }
+}
